fe83f9ce2a24ef52f309579a654417b743493556
Kakarot <joshuaeger@hypersoniclabs.co>
2025-11-20 18:35:15 -0600

    Update light

--- docs/client/constructor/Actor/light.lua
+++ docs/client/constructor/Actor/light.lua
@@ -3,10 +3,10 @@ local Light = {}
 
 ---Spawns a new light actor and wraps its component.
 ---All component methods/properties are available directly on the wrapper.
----@param Location FVector @[opt] World position (default: 0,0,0)
----@param Rotation FRotator @[opt] World orientation (default: 0,0,0)
----@param Color FLinearColor @[opt] Light color and opacity (default: white opaque)
----@param LightTypeParam string @[opt] One of LightType.Spot, .Point, .Rect (default: Point)
+---@param Location FVector World position (default: 0,0,0)
+---@param Rotation FRotator World orientation (default: 0,0,0)
+---@param Color FLinearColor Light color and opacity (default: white opaque)
+---@param LightTypeParam string One of LightType.Spot, .Point, .Rect (default: Point)
 ---@param Intensity number @[opt] Light intensity (default: 5000)
 ---@param AttenuationRadius number @[opt] Radius for attenuation (Point & Spot only, default: 1000)
 ---@param ConeAngle number @[opt] Outer cone angle for spot lights (Spot only, default: 44)
