336be04705217e95f766a84084d0530540f0a86d
Kakarot <joshuaeger@hypersoniclabs.co>
2025-11-20 18:14:53 -0600

    decal test

---
+++ docs/client/constructor/Actor/decal.lua
@@ -0,0 +1,9 @@
+---Creates a new Decal actor in the world
+---@param Location FVector World position
+---@param Rotation FRotator World orientation
+---@param MaterialAsset string Long package name, e.g. "/Game/Decals/MyDecalMat.MyDecalMat"
+---@param Size FVector @[opt] Decal size (default: 128×256×256)
+---@param Lifespan number @[opt] Seconds to live (default: 60)
+---@param FadeScreenSize number @[opt] Screen-size at which it fades (default: 0.01)
+---@return table Wrapper with .Object (AActor) and .Component (UDecalComponent)
+function Decal(Location, Rotation, MaterialAsset, Size, Lifespan, FadeScreenSize) end
